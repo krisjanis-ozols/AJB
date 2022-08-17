548c87855da11abab9c428f55f1d4370bf37340f
Krisjanis Ozols <krisjanis9@gmail.com>
2022-08-17 14:40:49 +0300

    3rd and 4th tasks

---
+++ src/ajb2_3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ajb2_3 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int a;
+
+        System.out.println("Input whole number:");
+        a=sc.nextInt();
+        System.out.println("even -"+ (a%2==0));
+
+
+
+    }
+}
---
+++ src/ajb2_4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ajb2_4 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int a;
+
+        System.out.println("Input whole number:");
+        a=sc.nextInt();
+        System.out.println("odd -"+ (a%2==1));
+
+
+
+    }
+}
