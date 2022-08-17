446d8ac3b778e4cf50ae341202056f4b5bacc9a1
Krisjanis Ozols <krisjanis9@gmail.com>
2022-08-17 14:47:19 +0300

    5th tasks

---
+++ src/ajb2_5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ajb2_5 {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        boolean a;
+
+        System.out.println("Enter boolean value:");
+        a=sc.nextBoolean();
+        System.out.println("Oppposite of \""+a+"\" is \""+ !a+"\"");
+
+
+
+    }
+}
