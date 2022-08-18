2483f4b2d2a13f010a491b5c17aa33b47b4cab15
Krisjanis Ozols <krisjanis9@gmail.com>
2022-08-18 11:31:02 +0300

    Day 3 1st and 2nd tasks

---
+++ src/ajb3_1.java
@@ -0,0 +1,28 @@
+public class ajb3_1 {
+//    /1) Write a program which creates two dimensional
+//    // array and stores a multiplication table (from 1 - 10) in it.
+//    Example of output:
+//            1 * 1 = 1
+//            1 * 2 = 2
+//            1 * 3 = 3
+//            ....
+//            10 * 9 = 90
+//            10 * 10 = 100
+//    Hint:
+//            1) don't forget that you have to save the values in the array and output them from the array.
+//            2) Arrays should be the size of [10][10]
+    public static void main(String[] args){
+        int[][] arr = new int[10][10];
+
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
+
+                arr[i][j]=(i+1)*(j+1);
+                System.out.println(i+1+" * "+ (j+1) + " = "+arr[i][j]);
+
+            }
+        }
+    }
+
+
+}
---
+++ src/ajb3_2.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class ajb3_2 {
+
+//    2) Declare and initialize an array with four arbitrary whole numbers.
+//    // Write a Java program to copy this array by iterating it.
+
+    public static void main(String[] args){
+
+        Random r = new Random();
+        int[] arr = new int[4];
+        System.out.println("Original:");
+        for(int i=0;i<4;i++){
+            arr[i]=r.nextInt(100);
+            System.out.println(arr[i]);
+        }
+        int[] arr2 = new int[arr.length];
+
+        System.out.println("Copy:");
+        for(int i=0; i<arr.length; i++){
+            arr2[i]=arr[i];
+            System.out.println(arr2[i]);
+        }
+
+
+    }
+
+}
